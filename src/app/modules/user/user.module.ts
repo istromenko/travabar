ea1391dcb35b615b0756f383ede3e0a2a51e0d44
IlyaStromenko <iistromenko@gmail.com>
2022-06-09 15:56:25 +0300

    add base tab navigation for users

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { UserRoutingModule } from "./user-routing.module";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatIconModule } from "@angular/material/icon";
 
 
 
@@ -12,6 +14,9 @@ import { UserRoutingModule } from "./user-routing.module";
   imports: [
     CommonModule,
     UserRoutingModule,
-  ]
+    MatTabsModule,
+    MatIconModule,
+  ],
+  bootstrap: [UserComponent],
 })
 export class UserModule { }
