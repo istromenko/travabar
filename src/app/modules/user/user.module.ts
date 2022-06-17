7e2871a0e26bf45a48eeb7791ffb0fc174603c6e
IlyaStromenko <iistromenko@gmail.com>
2022-06-17 09:55:54 +0300

    add import product card into user page

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -5,20 +5,23 @@ import { UserRoutingModule } from "./user-routing.module";
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatIconModule } from "@angular/material/icon";
 import { ClipsMultiSelectModule } from "../../components/chips-multi-select/clips-multi-select.module";
-
+import { ProductCardModule } from "../../components/product-card/product-card.module";
 
 
 @NgModule({
   declarations: [
     UserComponent,
   ],
-    imports: [
-        CommonModule,
-        UserRoutingModule,
-        MatTabsModule,
-        MatIconModule,
-        ClipsMultiSelectModule,
-    ],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    MatTabsModule,
+    MatIconModule,
+    ClipsMultiSelectModule,
+    ProductCardModule,
+
+  ],
   bootstrap: [UserComponent],
 })
-export class UserModule { }
+export class UserModule {
+}
